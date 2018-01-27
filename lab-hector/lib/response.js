8d76919c546f2385a77782c07b19c2c1901845a0
Hector <norzagaray@me.com>
2018-01-26 18:16:05 -0800

    scaffold and ready for work

---
+++ lab-hector/lib/response.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Just for example purposes and check and compare with my previous lab
+const response = module.exports = {};
+
+response.json = function (status, data, res) {
+
+};
+
+response.text = function (status, message, res) {
+
+};
